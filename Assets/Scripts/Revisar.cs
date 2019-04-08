bc2c49473d83b2a412bbe78d5dab15a3b6831115
Cooconuts <45293763+Cooconnuts@users.noreply.github.com>
2019-04-08 13:00:55 +0200

    Lighting and Reflection probes

---
+++ Assets/Scripts/Revisar.cs
@@ -0,0 +1,38 @@
+﻿/* DUDAS
+ * 
+ * ¿Qué Lightmap Resolution, Padding y Size es recomendable poner?
+ * 
+ * ¿Tengo que bajar la resolución de todos los archivos de las texturas para bajarle el peso? 
+ * (algunas texturas están en 4K) 
+ * ¿O sólo bajo la resolución de los archivos que más pesen? 
+ * Son muchas texturas como para ir 1 a 1, si no hay otra manera pues se hace.
+ * 
+ * ¿Qué hago con el exterior? ¿Creo una pequeña zona residencial para que no se quede la casa en el vacío?
+ * ¿O qué otra cosa podría hacer? (sin que resulte en añadir muchos más elementos para no cargar la escena
+ */
+
+/* PROBLEMAS
+ * 
+ * Cuando hago el baking de las luces se me hace muy raro y queda feo como si estuviera a muy baja resolución o algo
+ * 
+ * He puesto que todos los prefabs de las paredes, suelos y techos generen sombras a ambos lados y algunas luces siguen traspasándolos
+ * 
+ * Me salen unas especie de sombras en algunas paredes y techos cuando la luz es más o menos intensa y no se si será por 
+ * lo de la resolución de las luces baked
+ * 
+ * Error Objetos con UVs sobrepuestas
+ * https://docs.unity3d.com/Manual/ProgressiveLightmapper-UVOverlap.html
+ * No me aparece el menú de Objects Maps en Lighting para corregir esas UVs
+ * 
+ */
+
+ /* OTRAS DUDAS
+  * 
+  * ¿Hay que entregar un dossier para la presentación del proyecto?
+  * 
+  * ¿Qué puntos importantes hay que añadir en el dossier?
+  * 
+  * 
+  * 
+  * 
+  */
