a70b8c76c38bca5ba93657de3261a67d04a15f12
Cooconuts <45293763+Cooconnuts@users.noreply.github.com>
2019-05-17 11:56:20 +0200

    Update
    - Oculus plugin
    - VRTK
    - Ajuste de luces
    - Organización de luces

---
+++ Assets/Scripts/Activar_Luz.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activar_Luz : MonoBehaviour
+{
+    Animator animacion;
+
+    public GameObject[] luces;
+
+
+    void Start()
+    {
+        animacion = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Revisar.cs
+++ Assets/Scripts/Revisar.cs
@@ -7,18 +7,28 @@
  * ¿O sólo bajo la resolución de los archivos que más pesen? 
  * Son muchas texturas como para ir 1 a 1, si no hay otra manera pues se hace.
  * 
- * ¿Qué hago con el exterior? ¿Creo una pequeña zona residencial para que no se quede la casa en el vacío?
- * ¿O qué otra cosa podría hacer? (sin que resulte en añadir muchos más elementos para no cargar la escena
+ * 
+ * ¿Las luces que se enciendan independientemente las meto también en objetos vacíos?
+ * 
+ * ¿Hay alguna manera de activar la emision del material de un objeto sin activarlo para el resto de objetos que tengan el mismo material?
+ * 
  */
 
 /* PROBLEMAS
  * 
- * Cuando hago el baking de las luces se me hace muy raro y queda feo como si estuviera a muy baja resolución o algo
  * 
  * He puesto que todos los prefabs de las paredes, suelos y techos generen sombras a ambos lados y algunas luces siguen traspasándolos
  * 
- * Me salen unas especie de sombras en algunas paredes y techos cuando la luz es más o menos intensa y no se si será por 
- * lo de la resolución de las luces baked
+ * De repente Unity compila constantemente los scripts aun que no haya hecho ningún cambio en ellos, aun que sea modificando cualquier cosa de la escena o editor 
+ *  se pone a compilar los scripts constantemente
+ *  
+ * A continuación
+ * 
+ * Al instalar SteamVR Plugin Unity instala constantemente el OpenVR, aun que lo desinstale lo vuelve a instalar inmediatamente. Lo único que soluciona esto es borrar
+ *  la carpeta de SteamVR
+ * 
+ * Despues de cada mínimo cambio Unity compila los scripts e instala de nuevo el OpenVR
+ * 
  * 
  * Error Objetos con UVs sobrepuestas
  * https://docs.unity3d.com/Manual/ProgressiveLightmapper-UVOverlap.html
@@ -26,13 +36,24 @@
  * 
  */
 
- /* OTRAS DUDAS
-  * 
-  * ¿Hay que entregar un dossier para la presentación del proyecto?
-  * 
-  * ¿Qué puntos importantes hay que añadir en el dossier?
-  * 
-  * 
-  * 
-  * 
-  */
+/* OTRAS DUDAS
+ * 
+ * ¿Qué hago con el exterior? ¿Creo una pequeña zona residencial para que no se quede la casa en el vacío?
+ * ¿O qué otra cosa podría hacer? (sin que resulte en añadir muchos más elementos para no cargar la escena)
+ * 
+ */
+
+/* SPRINTS
+ * 
+ * SPRINT 1: 17 ABRIL AL 01 MAYO 
+ * Prerrequisitos: gráficos y GDD (Objetivo y mecánicas)
+ * Tareas: tareas de programación básicas (mecánicas) y diseño de niveles
+ * 
+ * SPRINT 2: 02 MAYO AL 15 MAYO
+ * Prerrequisitos: Diseño de niveles terminado
+ * Tareas: Programación avanzada e iluminación
+ * 
+ * SPRINT 3: 16 MAYO AL 29 MAYO
+ * Prerrequisitos: Mecánicas terminadas
+ * Tareas: Testeo y optimizacion y sonido
+ */
