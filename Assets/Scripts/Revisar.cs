2507a5731b027f707e92474cecdadcafffaa1fed
Cooconuts <45293763+Cooconnuts@users.noreply.github.com>
2019-05-21 01:07:46 +0200

    Update
    - Configuración de OVR y simulador
    - Retoque de assets
    -

--- Assets/Scripts/Revisar.cs
+++ Assets/Scripts/Revisar.cs
@@ -1,6 +1,9 @@
-﻿/* DUDAS
+﻿/* ---------- DUDAS ----------
  * 
- * ¿Qué Lightmap Resolution, Padding y Size es recomendable poner?
+ * ¿Para la interfaz hay que tener algo en cuenta? ¿Se puede hacer plana para ponerla en una pared o algo o tiene que ser en 3D para VR sí o sí?
+ * 
+ * ¿En el simulador deberia haber colisiones con los objetos? Lo traspaso todo 
+ * 
  * 
  * ¿Tengo que bajar la resolución de todos los archivos de las texturas para bajarle el peso? 
  * (algunas texturas están en 4K) 
@@ -8,16 +11,14 @@
  * Son muchas texturas como para ir 1 a 1, si no hay otra manera pues se hace.
  * 
  * 
- * ¿Las luces que se enciendan independientemente las meto también en objetos vacíos?
- * 
- * ¿Hay alguna manera de activar la emision del material de un objeto sin activarlo para el resto de objetos que tengan el mismo material?
- * 
  */
 
-/* PROBLEMAS
- * 
+/* ---------- PROBLEMAS ----------
  * 
  * He puesto que todos los prefabs de las paredes, suelos y techos generen sombras a ambos lados y algunas luces siguen traspasándolos
+ * 
+ * No puedo coger los objetos, como si no se detectaran los controladores, los objetos no reaccionan a estos
+ * 
  * 
  * De repente Unity compila constantemente los scripts aun que no haya hecho ningún cambio en ellos, aun que sea modificando cualquier cosa de la escena o editor 
  *  se pone a compilar los scripts constantemente
@@ -34,16 +35,18 @@
  * https://docs.unity3d.com/Manual/ProgressiveLightmapper-UVOverlap.html
  * No me aparece el menú de Objects Maps en Lighting para corregir esas UVs
  * 
+ * 
  */
 
-/* OTRAS DUDAS
+/* ---------- OTRAS DUDAS ----------
  * 
  * ¿Qué hago con el exterior? ¿Creo una pequeña zona residencial para que no se quede la casa en el vacío?
  * ¿O qué otra cosa podría hacer? (sin que resulte en añadir muchos más elementos para no cargar la escena)
  * 
+ * 
  */
 
-/* SPRINTS
+/* ---------- SPRINTS ----------
  * 
  * SPRINT 1: 17 ABRIL AL 01 MAYO 
  * Prerrequisitos: gráficos y GDD (Objetivo y mecánicas)
@@ -56,4 +59,6 @@
  * SPRINT 3: 16 MAYO AL 29 MAYO
  * Prerrequisitos: Mecánicas terminadas
  * Tareas: Testeo y optimizacion y sonido
+ * 
+ * 
  */
