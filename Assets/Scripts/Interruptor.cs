f1df1ec9580ed13e3b68781994be0570a5bb8505
Cooconnuts <45293763+Cooconnuts@users.noreply.github.com>
2019-05-24 17:15:35 +0200

    Update
    - Animaciones de luces

---
+++ Assets/Scripts/Interruptor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interruptor : MonoBehaviour
+{
+    public Animator luz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Usar()
+    {
+        luz.SetTrigger("activar");
+    }
+}
