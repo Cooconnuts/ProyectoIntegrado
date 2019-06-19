4298649c993038c9b5d36efdadb8314157865e38
Cooconuts <45293763+Cooconnuts@users.noreply.github.com>
2019-06-19 17:04:33 +0200

    Update
    Script de revisar actualizado

--- Assets/Scripts/Revisar.cs
+++ Assets/Scripts/Revisar.cs
@@ -2,15 +2,9 @@
  * 
  * EN CASA
  * 
- * Programar el cuadro de luces (hacer que funcione al activarlo y desactivarlo, no solo al activarlo la primera vez)
+ * Añadir panel con los controles
  * 
- * Acabar la interfaz y hacerla funcional (testing)
- * 
- * Conteo de luces
- * 
- * Añadir movil para buscar, ponerlo en el baño de abajo
- * https://www.turbosquid.com/3d-models/3ds-max-6-apple-iphone/854423
- * 
+ * Hacer un botón para salir del juego
  * 
  * 
  * EN LA ESCAV
@@ -25,59 +19,32 @@
 
 /* ---------- DUDAS ----------
  * 
- * Como hacer el conteo las luces que se han encendido (*cuál*opcional y cuantas veces) y el tiempo que están encendidas.
+ * Estados del cuadro de luces
  * 
- * Como hacer para que el script del Interruptor esté en distintos estados dependiendo de la escena
  * Menú principal: no hay cuadro de luces, por lo que o quitar el booleano o hacer que empiece en true 
- * Nivel bajo: que el booleano empiece en true por que en este nivel no hay que activarlo
- * Nivel alto: tal como está
+ * Nivel bajo: que el booleano empiece en true para que las luces se puedan activar desde el principio
+ * Nivel alto: que el booleano empiece en false ya que en este nivel hay que activarlo para hacer funcionar las luces
  * 
- * Como hacer que los botones de la interfaz reaccionen al puntero
- *
+ * El teléfono móvil está en el baño de la primera planta, ¿cómo hago para que cuando se coja el movil aparezca el panel con la información?
+ * ¿Puedo hacerlo como un objeto interactivo y que al cogerse o al usarse hacer que aparezca el panel con el SetActive?
  * 
- * Cómo incorporo lo del panel que sale al acabar con la info de las luces?
  * 
  * 
  * ¿Cómo hacer para entregarlo? Aumento la resolución de las luces y sombras para hacer el ejecutable?
- * 
- * Para abrir las puertas, ¿puedo limitar el ángulo en el que se abre la puerta como máximo?
- * https://www.moddb.com/tutorials/how-to-open-a-vr-door-in-unity-and-steamvr
  * 
  */
 
 /* ---------- PROBLEMAS ----------
  * 
- * Al descargar del github el proyecto se me pierden los materiales de las lámparas cuando están apagadas habiéndolas puesto bien antes
+ * Cuando cargo una escena desde el menu principal con las gafas no me deja moverme, es como si estuviera atascado en algo o como si no reconociese los mandos 
  * 
- * Cuando utilizo las gafas en la escuela, se me cambia la altura del avatar y aun que lo baje se me queda a la misma altura, ¿en algún sitio se puede cambiar la altura?
- * 
- * 
- * Cuando cargo una escena desde el menu principal con las gafas no me deja moverme 
  * Los botones no cambian de color cuando el puntero los apunta, sí carga las escenas pero no se resalta de ninguna manera para dar a entender al jugador que es un boton.
+ * ¿Cómo puedo hacer que el botón se resalte? Con las opciones que tienen los botones de la UI no parece funcionar cuando se interactua con el puntero, ¿a lo mejor debería hacerlo objeto interactivo?
  * 
  * Algo ha cambiado y el cuadro de luces ya no funciona como debería, ahora las luces se encienden independientemente de si el cuadro de luces está activo o no
  * 
- * 
- * Error Objetos con UVs sobrepuestas
- * https://docs.unity3d.com/Manual/ProgressiveLightmapper-UVOverlap.html
- * No me aparece el menú de Objects Maps en Lighting para corregir esas UVs
- * 
- * 
  */
 
-/* ---------- OTRAS DUDAS ----------
- * 
- * ¿Qué hago con el exterior? ¿Creo una pequeña zona residencial para que no se quede la casa en el vacío?
- * ¿O qué otra cosa podría hacer? (sin que resulte en añadir muchos más elementos para no cargar la escena)
- * 
- * 
- * ¿Tengo que bajar la resolución de todos los archivos de las texturas para bajarle el peso? 
- * (algunas texturas están en 4K) 
- * ¿O sólo bajo la resolución de los archivos que más pesen? 
- * Son muchas texturas como para ir 1 a 1, si no hay otra manera pues se hace.
- * 
- * 
- */
 
 /* ---------- SPRINTS ----------
  * 
